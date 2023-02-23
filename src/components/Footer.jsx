24c55f0ff243d312951330d9c5f1be1518e93ca4
Sagar Kapoor <sagrkapr@gmail.com>
2023-02-23 07:20:48 +0200

    🚀 Project Initiated

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="text-white bg-black mt-60">
+      <div>
+        <h1 className="text-4xl font-bold">Capture</h1>
+        <ul>
+          <li>Home</li>
+          <li>Gallerry</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
+import { Raleway } from "@next/font/google";
+const raleway = Raleway({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={raleway.className}>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </main>
+  );
 }
