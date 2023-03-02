10cda1e189f5a269520706c865d0ec99bbe86821
Sagar Kapoor <sagrkapr@gmail.com>
2023-03-02 14:48:16 +0200

    ⚡️ Added axios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,12 +14,24 @@ const Footer = () => {
       <h1 className="p-5 text-3xl font-bold text-center">Capture</h1>
       <div className="flex flex-col items-center">
         <div className="flex flex-row gap-5 mb-5">
-          <FaLinkedin
-            size="20"
-            className="transition-all duration-300 hover:transform hover:scale-110"
-          />
-          <FaGithub size="20" />
-          <FaVolleyballBall size="20" />
+          <a href="https://www.linkedin.com/in/sagarkapr/">
+            <FaLinkedin
+              size="20"
+              className="transition-all duration-300 cursor-pointer hover:transform hover:scale-110"
+            />
+          </a>
+          <a href="https://github.com/sagar-Kap/">
+            <FaGithub
+              size="20"
+              className="transition-all duration-300 cursor-pointer hover:transform hover:scale-110"
+            />
+          </a>
+          <a href="https://sagarkapoor.eu/">
+            <FaVolleyballBall
+              size="20"
+              className="transition-all duration-300 cursor-pointer hover:transform hover:scale-110"
+            />
+          </a>
         </div>
         <p className="mb-5 text-xs">All rights reserved ©2020-{year} </p>
       </div>
