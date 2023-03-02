2343f906715bfd7026fafff50a1ec540d0ec1dce
Sagar Kapoor <sagrkapr@gmail.com>
2023-03-02 08:03:03 +0200

    ✨ Added Contact Form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,30 @@
+import {
+  FaLinkedin,
+  FaGithub,
+  FaSitemap,
+  FaVolleyballBall,
+} from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div className="text-white bg-black">
-      <div>
+    <div className="flex flex-col justify-between p-10 text-white bg-black sm:flex-row">
+      <div className="flex flex-col gap-4 ">
         <h1 className="text-4xl font-bold">Capture</h1>
+
         <ul>
-          <li>Home</li>
+          <li class="after:content-[`>`]">Home </li>
           <li>Gallerry</li>
           <li>Work</li>
           <li>Contact</li>
         </ul>
+      </div>
+      <div className="mt-5">
+        <p>© All rights reserved</p>
+        <div className="flex flex-row gap-5 mt-5 sm:mt-9">
+          <FaLinkedin size="20" />
+          <FaGithub size="20" />
+          <FaVolleyballBall size="20" />
+        </div>
       </div>
     </div>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,30 +1,14 @@
 import Hero from "@/components/Hero";
-import { TextField, Button } from "@mui/material";
-import { Container } from "@mui/system";
+import Contact from "@/components/Contact";
 
 const contact = () => {
   return (
     <div>
       <Hero
         heading="Contact Us"
-        message="Reach out to us to so that we can work together on an optimum solution for your business."
+        message="Let's collaborate to launch your idea!"
       />
-      <form className="flex flex-col items-center" onSubmit>
-        <ul>
-          <li>
-            <label for="name">Name</label>
-            <input type="text" required />
-          </li>
-          <li>
-            <label for="email">Email</label>
-            <input type="email" />
-          </li>
-          <li>
-            <label for="message">Message</label>
-            <textarea name="user_msg" id="message_by_user"></textarea>
-          </li>
-        </ul>
-      </form>
+      <Contact />
     </div>
   );
 };
