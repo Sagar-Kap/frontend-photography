6add7229bc5a04a421d561ae75c6d0dec6ec53b2
Sagar Kapoor <sagrkapr@gmail.com>
2023-03-06 09:28:47 +0200

    ✨ Hero section made smaller for sm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = ({ message, heading }) => {
   return (
-    <div className="relative flex bg-fixed  justify-center  bg-cover  bg-center items-center h-screen text-white bg-[url('../../public/hero.png')]">
+    <div className="relative flex bg-fixed  justify-center  bg-cover  bg-center items-center h-[600px] sm:h-screen text-white bg-[url('../../public/hero.png')]">
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/70 z-[2]" />
       <div className="z-[2] ml-5 sm:ml-[-10rem]">
         <h1 className="text-5xl font-bold">{heading}</h1>
